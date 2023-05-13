a160bd43bff307ff449274a841672005d7c28156
Pawel Zguda <zgudapawel@gmail.com>
2023-05-14 01:40:19 +0200

    update rotes

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,48 @@ import {
   createRoutesFromElements,
 } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './pages/Home';
-import About from './pages/About';
 import Settings from './components/Settings';
 import UserPlaces from './components/UserPlaces';
 import NewPlace from './components/NewPlace';
 import UpdatePlace from './components/UpdatePlace';
 import Login from './components/Login';
+import Users from './pages/Users';
 
 function App() {
   const [logged, setLogged] = useState(false);
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/ustawienia" element={<Settings />} />
-        <Route path="/:userId/miejsca" element={<UserPlaces />} />
-        <Route path="/miejsca/nowe" element={<NewPlace />} />
-        <Route path="/miejsca/:placeId" element={<UpdatePlace />} />
-        <Route path="/login" element={<Login />} />
-      </Route>
+
+
+
+   let router
+   
+   if(logged) {
+    router = createBrowserRouter(
+      createRoutesFromElements(
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Users />} />
+          <Route path="/ustawienia" element={<Settings />} />
+          <Route path="/:userId/miejsca" element={<UserPlaces />} />
+          <Route path="/miejsca/nowe" element={<NewPlace />} />
+          <Route path="/miejsca/:placeId" element={<UpdatePlace />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+      )
+    );
+   } else {
+    router = createBrowserRouter(
+      createRoutesFromElements(
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Users />} />
+          <Route path="/:userId/miejsca" element={<UserPlaces />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+      )
     )
-  );
+   }
+   
+
+
 
   return <RouterProvider router={router} />;
 }
---
+++ src/pages/Users.jsx
@@ -0,0 +1,6 @@
+
+export default function Users() {
+  return (
+    <div>Uzytkownicy</div>
+  )
+}
