266b30550914a265ea7a4485fbe9a10343db01d5
Pawel Zguda <zgudapawel@gmail.com>
2023-05-15 15:18:34 +0200

    add burger menu

--- src/shared/sharedComponents/navigation/MainNavigation.jsx
+++ src/shared/sharedComponents/navigation/MainNavigation.jsx
@@ -1,19 +1,43 @@
-// import React from 'react'
+import { useState } from 'react';
 import css from './MainNavigation.module.css';
 
 import MainHeader from './MainHeader';
 import { Link } from 'react-router-dom';
 import NavLinks from './NavLinks';
 
-export default function MainNavigation() {
+const MainNavigation = () => {
+  const [sideMenuIsOpen, setSideMenuIsOpen] = useState(false);
+
+  const openSideMenu = () => {
+    setSideMenuIsOpen(true);
+  };
+  const closeSideMenu = () => {
+    setSideMenuIsOpen(false);
+  };
+
   return (
-    <MainHeader>
-      <h1 className={css.title}>
-        <Link to="/">travelGoals</Link>
-      </h1>
-      <nav className={css.headerNav}>
+    <>
+      {sideMenuIsOpen && (
+        <nav className={css.sideMenuNav}>
           <NavLinks />
         </nav>
-    </MainHeader>
+      )}
+      <MainHeader>
+        <button className={css.menuBtn} onClick={openSideMenu}>
+          <i
+            className={`fa fa-bars ${css.sideMenuIcon}`}
+            aria-hidden="true"
+          ></i>
+        </button>
+        <h1 className={css.title}>
+          <Link to="/">travelGoals</Link>
+        </h1>
+        <nav className={css.headerNav}>
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
   );
-}
+};
+
+export default MainNavigation;
