a4caf0faf416f6514ed5a5a93e5ba4f54267614d
Pawel Zguda <zgudapawel@gmail.com>
2023-05-24 15:21:15 +0200

    adding some state and related to it conditional rendering in Users comp.

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
-
-   const USERS = [
-         {
-            id: 'u1',
-            name: 'Pawel Zguda',
-            image: 'https://images.pexels.com/photos/4095246/pexels-photo-4095246.jpeg',
-            places: 3,
-        },
-         {
-            id: 'u2',
-            name: 'Tomasz Rydel',
-            image: 'https://images.pexels.com/photos/4095246/pexels-photo-4095246.jpeg',
-            places: 2,
-        },
-     ];
-
+const USERS = [
+  {
+    id: 'u1',
+    name: 'Pawel Zguda',
+    image: 'https://images.pexels.com/photos/4095246/pexels-photo-4095246.jpeg',
+    places: 3,
+  },
+  {
+    id: 'u2',
+    name: 'Tomasz Rydel',
+    image: 'https://images.pexels.com/photos/4095246/pexels-photo-4095246.jpeg',
+    places: 2,
+  },
+];
 
 const Users = () => {
-  return <div>Uzytkownicy</div>;
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedUsers, setLoadedUsers] = useState();
+
+  useEffect(() => {
+    // here later adding function that fetches data
+    const fetchUsers = () => {
+      return setLoadedUsers(USERS);
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      {isLoading && (
+        <div className="center">
+          <p>ladowanie danych</p>
+        </div>
+      )}
+      {!isLoading && loadedUsers && <p>zaladowano uzytkownikow</p>}
+    </>
+  );
 };
 
 export default Users;
