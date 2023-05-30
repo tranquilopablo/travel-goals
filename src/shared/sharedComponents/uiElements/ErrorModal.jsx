e1f8480f421740e30ffddeb6e8aaee67f5d6f231
Pawel Zguda <zgudapawel@gmail.com>
2023-05-30 15:19:51 +0200

    update modal comp

--- src/shared/sharedComponents/uiElements/ErrorModal.jsx
+++ src/shared/sharedComponents/uiElements/ErrorModal.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import Modal from './Modal';
 
 const ErrorModal = (props) => {
-  return <Modal><p>{props.error}</p></Modal>;
+  return (
+    <Modal
+      onCancel={props.onClear}
+      header="Coś poszło nie tak!"
+      show={!!props.error}
+      footer={<button onClick={props.onClear}>Okej</button>}
+    >
+      <p>{props.error}</p>
+    </Modal>
+  );
 };
 
 export default ErrorModal;
--- src/shared/sharedComponents/uiElements/Modal.jsx
+++ src/shared/sharedComponents/uiElements/Modal.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Backdrop from './Backdrop';
+import ReactDOM from 'react-dom';
+import css from './Modal.module.css';
+
+const ModalOverlay = (props) => {
+  const content = (
+    <div className={css.modal}>
+      <header className={css.modalHeader}>
+        <h2>{props.header}</h2>
+      </header>
+      <form>
+        <div className={css.modalContent}>{props.children}</div>
+        <footer className={css.modalFooter}>{props.footer}</footer>
+      </form>
+    </div>
+  );
+  return ReactDOM.createPortal(content, document.getElementById('modal-hook'));
+};
 
 const Modal = (props) => {
   return (
-    <div>{props.children}</div>
-  )
-}
+    <>
+      {props.show && <Backdrop onClick={props.onCancel} />}
+      <ModalOverlay {...props} />
+    </>
+  );
+};
 
-export default Modal
+export default Modal;
