db5e917420c0b9e0ed30a8d994d37811fda51f9e
Pawel Zguda <zgudapawel@gmail.com>
2023-06-13 15:26:39 +0200

    update userPlaces comp

--- src/pages/UserPlaces.jsx
+++ src/pages/UserPlaces.jsx
@@ -1,4 +1,17 @@
+import { useState } from 'react';
+import ErrorModal from '../shared/sharedComponents/uiElements/ErrorModal';
 
 export default function UserPlaces() {
-  return <div>UserPlaces</div>;
+  const [error, setError] = useState(false);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={errorHandler} />
+      <div>UserPlacesss</div>
+    </>
+  );
 }
