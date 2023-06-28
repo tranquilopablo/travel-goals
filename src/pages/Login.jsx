3b5309cb68a4694ff4c2ada42a2dcf3a8ce1496b
Pawel Zguda <zgudapawel@gmail.com>
2023-06-28 15:25:54 +0200

    update login comp

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,36 @@
 import React, { useState } from 'react';
+import Button from '../shared/sharedComponents/uiElements/Button';
 import Card from '../shared/sharedComponents/uiElements/Card';
+import ErrorModal from '../shared/sharedComponents/uiElements/ErrorModal';
+import LoadingSpinner from '../shared/sharedComponents/uiElements/LoadingSpinner';
+import css from './Login.module.css';
 
-export default function Login() {
+
+const Login = () => {
   const [isLoginMode, setIsLoginMode] = useState(true);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  const switchModeHandler = () => {
+    setIsLoginMode((prevMode) => !prevMode);
+  };
 
   return (
-    <Card>
-      <h2>LOGIN WYMAGANY</h2>
-    </Card>
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      <Card className={css.authentication}>
+        {isLoading && <LoadingSpinner asOverlay />}
+        <h2>LOGIN WYMAGANY</h2>
+        <Button inverse onClick={switchModeHandler}>
+          PRZEJDŻ DO {isLoginMode ? 'REJESTRACJI' : 'LOGOWANIA'}
+        </Button>
+      </Card>
+    </>
   );
-}
+};
+
+export default Login;
