1b09cc299909c67dec344f75bd1ce9690723e36b
Pawel Zguda <zgudapawel@gmail.com>
2023-07-03 15:19:25 +0200

    add formik into login comp

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,13 +2,41 @@ import React, { useState } from 'react';
 import Button from '../shared/sharedComponents/uiElements/Button';
 import Card from '../shared/sharedComponents/uiElements/Card';
 import ErrorModal from '../shared/sharedComponents/uiElements/ErrorModal';
+// import Input from '../shared/sharedComponents/uiElements/Input';
 import LoadingSpinner from '../shared/sharedComponents/uiElements/LoadingSpinner';
 import css from './Login.module.css';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const Login = () => {
   const [isLoginMode, setIsLoginMode] = useState(true);
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+
+  const signupSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    lastName: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
+    validationSchema: { signupSchema },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+      console.log('zalogowano!');
+    },
+  });
 
   const clearError = () => {
     setError(null);
@@ -18,12 +46,6 @@ const Login = () => {
     setIsLoginMode((prevMode) => !prevMode);
   };
 
-  const authsubmitHandler = async (e) => {
-    e.preventDefault();
-    console.log('zalogowano!');
-  };
-
-
   /// uzyc tutaj formik
   return (
     <>
@@ -32,17 +54,47 @@ const Login = () => {
         {isLoading && <LoadingSpinner asOverlay />}
         <h2>LOGIN WYMAGANY</h2>
         <hr />
-        <form onSubmit={authsubmitHandler}>
-          {!isLoginMode && <input />}
-          {/* {!isLoginMode && (
-            <img src="https://images.pexels.com/photos/4095246/pexels-photo-4095246.jpeg" />
-          )} */}
-          <input />
-          <input />
-          <Button type="submit">
-            {isLoginMode ? 'ZALOGUJ' : 'REJESTRACJA'}
-          </Button>
-        </form>
+        {/* <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            id="firstName"
+            name="firstName"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.firstName}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.firstName && formik.errors.firstName ? (
+            <p>{formik.errors.firstName}</p>
+          ) : null}
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            id="lastName"
+            name="lastName"
+            type="text"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.lastName}
+          />
+          {formik.touched.lastName && formik.errors.lastName ? (
+            <p>{formik.errors.lastName}</p>
+          ) : null}
+
+          <label htmlFor="email">Email Address</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <p>{formik.errors.email}</p>
+          ) : null}
+
+          <button type="submit">Submit</button>
+        </form> */}
         <Button inverse onClick={switchModeHandler}>
           PRZEJDŻ DO {isLoginMode ? 'REJESTRACJI' : 'LOGOWANIA'}
         </Button>
