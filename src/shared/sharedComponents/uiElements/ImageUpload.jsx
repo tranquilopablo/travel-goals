478278ee3fa2c6e1a21cf26c68d7152c93c8c3af
Pawel Zguda <zgudapawel@gmail.com>
2023-07-06 14:44:06 +0200

    update imageupload

--- src/shared/sharedComponents/uiElements/ImageUpload.jsx
+++ src/shared/sharedComponents/uiElements/ImageUpload.jsx
@@ -3,7 +3,24 @@ import css from './ImageUpload.module.css';
 import Button from './Button';
 
 const ImageUpload = (props) => {
+  const [file, setFile] = useState();
+  const [previewUrl, setPreviewUrl] = useState(
+    'https://images.pexels.com/photos/4095246/pexels-photo-4095246.jpeg'
+  );
+  const [isValid, setIsValid] = useState(false);
+
   const filePickerRef = useRef();
+
+  useEffect(() => {
+    if (!file) {
+      return;
+    }
+    setPreviewUrl(file);
+  }, [file]);
+
+  const pickedHandler = (event) => {
+    setFile(event.target.files[0]);
+  };
 
   const pickImageHandler = () => {
     filePickerRef.current.click();
@@ -11,7 +28,19 @@ const ImageUpload = (props) => {
 
   return (
     <div className={css['form-control']}>
-      <input ref={filePickerRef} style={{ display: 'none' }} />
+      <input
+        ref={filePickerRef}
+        style={{ display: 'none' }}
+        type="file"
+        accept=".jpg,.png,.jpeg"
+        onChange={pickedHandler}
+      />
+      <div>
+        <div>
+          <img src={previewUrl} alt="Podgląd" />
+          <p>Proszę wybierz zdjęcie.</p>
+        </div>
+      </div>
       <Button type="button" onClick={pickImageHandler}>
         WYBIERZ ZDJĘCIE
       </Button>
